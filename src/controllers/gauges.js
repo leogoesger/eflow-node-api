6d0db466b62d7dd3a5aded5d28a2ec5b2e7e4a5b
Leo Q <leoq91@gmail.com>
2018-02-24 00:36:36 -0800

    function to get file names

--- src/controllers/gauges.js
+++ src/controllers/gauges.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const Gauge = require('../models').Gauge;
 const Year = require('../models').Year;
 
@@ -14,11 +15,15 @@ module.exports = {
       .catch(err => res.status(400).send(err));
   },
 
-  show(req, res) {
-    return Gauge.findById(req.params.gaugeId, {
-      include: [{model: Year, as: 'years'}],
-    })
-      .then(gauge => res.status(200).send(gauge))
-      .catch(err => res.status(400).send(err));
+  async show(req, res) {
+    const response = await axios.get(
+      'https://eflow.nyc3.digitaloceanspaces.com/2_23_annual_flow_result/10255800_annual_result_matrix.csv'
+    );
+    return res.status(201).send(response.data);
+    // return Gauge.findById(req.params.gaugeId, {
+    //   include: [{model: Year, as: 'years'}],
+    // })
+    //   .then(gauge => res.status(200).send(gauge))
+    //   .catch(err => res.status(400).send(err));
   },
 };
--- src/utils/uploadToDatabase.js
+++ src/utils/uploadToDatabase.js
@@ -14,49 +14,20 @@ const AnnualFlow = require('../models').AnnualFlow;
 import regular_gauges from '../public/gaugeReference';
 import metricReference from '../public/metricReference';
 
-export const uploadFlowDataToDatabase2 = () => {
-  console.log('Uploading Flow Data to Database...'); // eslint-disable-line
-  AnnualFlow.destroy({where: {}}).then(() => {
-    let firstRow = true;
-    const result = {};
-    const mapping = {};
-    csv({
-      noheader: true,
-    })
-      .fromFile('src/public/10255800_annual_result_matrix.csv')
-      .on('csv', csvRow => {
-        if (firstRow) {
-          csvRow.forEach((ele, index) => {
-            result[ele] = [];
-            mapping[index] = ele;
-            firstRow = false;
-          });
-        } else {
-          csvRow.forEach((ele, index) => {
-            result[mapping[index]].push(ele);
-          });
-        }
-      })
-      .on('done', () => {
-        Object.keys(result).forEach(key => {
-          AnnualFlow.create({
-            year: key,
-            flowData: result[key],
-            gaugeId: 10255800,
-          });
-        });
-      });
+const _getFileNameList = dir => {
+  return new Promise(resolve => {
+    fs.readdir(dir, (err, filenames) => {
+      const nameList = filenames.reduce(filename => filename.includes('.csv'));
+      resolve(nameList);
+    });
   });
 };
 
-export const uploadFlowDataToDatabase = () => {
+export const uploadFlowDataToDatabase = async () => {
   console.log('Uploading Flow Data to Database...'); // eslint-disable-line
-  AnnualFlow.destroy({where: {}}).then(() => {
-    new Promise(resolve => {
-      fs.readdir('src/public/annual_flow_matrix', (err, filenames) => {
-        resolve(filenames);
-      });
-    }).then(filenames => {
+  try {
+    await AnnualFlow.destroy({where: {}});
+    fs.readdir('src/public/annual_flow_matrix', (err, filenames) => {
       filenames.forEach(filename => {
         if (!filename.includes('.csv')) {
           return;
@@ -92,61 +63,53 @@ export const uploadFlowDataToDatabase = () => {
           });
       });
     });
-  });
+  } catch (e) {
+    throw e;
+  }
 };
 
-export const uploadResultToDatabase = () => {
+export const uploadResultToDatabase = async () => {
   console.log('Uploading Result to Database...'); // eslint-disable-line
-  Gauge.destroy({where: {}}).then(() => {
-    Gauge.bulkCreate(regular_gauges)
-      .then(() => {
-        new Promise(resolve => {
-          return fs.readdir(
-            'src/public/annual_result_matrix',
-            (err, filenames) => {
-              resolve(filenames);
-            }
-          );
-        }).then(filenames => {
-          filenames.forEach(file => {
-            if (!file.includes('.csv')) {
-              return;
-            }
-            const csvFilePath = `src/public/annual_result_matrix/${file}`;
-            const current_result = {
-              Year: {},
-              AllYear: {},
-              Spring: {},
-              Summer: {},
-              Fall: {},
-              FallWinter: {},
-              Winter: {},
-            };
-            csv({
-              noheader: true,
-            })
-              .fromFile(csvFilePath)
-              .on('csv', csvRow => {
-                const seasonName = metricReference[`${csvRow[0]}`][0];
-                const dataEntryName = metricReference[`${csvRow[0]}`][1];
-
-                current_result[seasonName][dataEntryName] = csvRow.slice(1);
-                current_result[seasonName].gaugeId = Number(file.slice(0, -25));
-              })
-              .on('done', () => {
-                Year.create(current_result.Year);
-                AllYear.create(current_result.AllYear);
-                Spring.create(current_result.Spring);
-                Summer.create(current_result.Summer);
-                Fall.create(current_result.Fall);
-                FallWinter.create(current_result.FallWinter);
-                Winter.create(current_result.Winter);
-              });
+  try {
+    await Gauge.destroy({where: {}});
+    await Gauge.bulkCreate(regular_gauges);
+    fs.readdir('src/public/annual_result_matrix', (err, filenames) => {
+      filenames.forEach(file => {
+        if (!file.includes('.csv')) {
+          return;
+        }
+        const csvFilePath = `src/public/annual_result_matrix/${file}`;
+        const current_result = {
+          Year: {},
+          AllYear: {},
+          Spring: {},
+          Summer: {},
+          Fall: {},
+          FallWinter: {},
+          Winter: {},
+        };
+        csv({
+          noheader: true,
+        })
+          .fromFile(csvFilePath)
+          .on('csv', csvRow => {
+            const seasonName = metricReference[`${csvRow[0]}`][0];
+            const dataEntryName = metricReference[`${csvRow[0]}`][1];
+            current_result[seasonName][dataEntryName] = csvRow.slice(1);
+            current_result[seasonName].gaugeId = Number(file.slice(0, -25));
+          })
+          .on('done', () => {
+            Year.create(current_result.Year);
+            AllYear.create(current_result.AllYear);
+            Spring.create(current_result.Spring);
+            Summer.create(current_result.Summer);
+            Fall.create(current_result.Fall);
+            FallWinter.create(current_result.FallWinter);
+            Winter.create(current_result.Winter);
           });
-        });
-      })
-      .catch(err => {
-        return err;
       });
-  });
+    });
+  } catch (e) {
+    throw e;
+  }
 };
