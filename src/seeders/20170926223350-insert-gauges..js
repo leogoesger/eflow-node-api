ea82473a3ad19abae9ff4bacee1262b494cefcea
Leo Q <leoq91@gmail.com>
2018-02-27 11:53:51 -0800

    seeder for gauge and class

--- src/seeders/20170926223350-insert-gauges..js
+++ src/seeders/20170926223350-insert-gauges..js
@@ -1,11 +1,11 @@
-// 'use strict';
-// const {gauges} = require('../seeder-data');
+'use strict';
+const {gauges} = require('../seeder-data');
 
-// module.exports = {
-//   up: (queryInterface, _Sequelize) => {
-//     return queryInterface.bulkInsert('Gauges', gauges);
-//   },
-//   down: (queryInterface, _Sequelize) => {
-//     return queryInterface.bulkDelete('Gauges');
-//   },
-// };
+module.exports = {
+  up: (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert('Gauges', gauges);
+  },
+  down: (queryInterface, _Sequelize) => {
+    return queryInterface.bulkDelete('Gauges');
+  },
+};
