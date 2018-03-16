3c01195c7fc6d190ded2939166a89460a50cd265
Leo Q <leoq91@gmail.com>
2018-03-16 14:18:19 -0700

    FF-178: add gauge summar flow matrix

--- src/migrations/20171017035360-create-gauge.js
+++ src/migrations/20171017035360-create-gauge.js
@@ -24,6 +24,129 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
       },
+      Avg: {type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), allowNull: true}, // eslint-disable-line
+      Std: {type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), allowNull: true}, // eslint-disable-line
+      CV: {type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), allowNull: true}, // eslint-disable-line
+      SP_Tim: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SP_Mag: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SP_Dur: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SP_ROC: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SU_BFL_Tim: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SU_BFL_Mag_10: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SU_BFL_Mag_50: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SU_BFL_Dur_Fl: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SU_BFL_Dur_Wet: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      SU_BFL_No_Flow: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      FAFL_Tim: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      FAFL_Mag: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      FAFL_Tim_Wet: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      FAFL_Dur: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      Wet_BFL_Mag: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Tim_2: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Dur_2: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Fre_2: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Tim_5: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Dur_5: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Fre_5: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Tim_10: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Dur_10: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Fre_10: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Tim_20: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Dur_20: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Fre_20: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Tim_50: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Dur_50: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
+      WIN_Fre_50: {
+        type: Sequelize.ARRAY(Sequelize.DECIMAL(10, 2)), // eslint-disable-line
+        allowNull: true,
+      },
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
--- src/models/gauge.js
+++ src/models/gauge.js
@@ -24,6 +24,117 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
+    Avg: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    Std: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    CV: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    SP_Tim: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    SP_Mag: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    SP_Dur: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    SP_ROC: {type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), allowNull: true}, //eslint-disable-line
+    SU_BFL_Tim: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    SU_BFL_Mag_10: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    SU_BFL_Mag_50: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    SU_BFL_Dur_Fl: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    SU_BFL_Dur_Wet: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    SU_BFL_No_Flow: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    FAFL_Tim: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    FAFL_Mag: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    FAFL_Tim_Wet: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    FAFL_Dur: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    Wet_BFL_Mag: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Tim_2: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Dur_2: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Fre_2: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Tim_5: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Dur_5: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Fre_5: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Tim_10: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Dur_10: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Fre_10: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Tim_20: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Dur_20: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Fre_20: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Tim_50: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Dur_50: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
+    WIN_Fre_50: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL(10, 2)), //eslint-disable-line
+      allowNull: true,
+    },
   });
   Gauge.associate = models => {
     Gauge.belongsTo(models.Classification, {
