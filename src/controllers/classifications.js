0d1d7d607d9cc06ac63e51614d4c5f771a683db8
Leo Q <leoq91@gmail.com>
2018-04-20 15:55:39 -0700

    FF-191: percentile data for classes

--- src/controllers/classifications.js
+++ src/controllers/classifications.js
@@ -1,120 +1,5 @@
-import * as d3 from 'd3';
-import {
-  Classification,
-  Hydrograph,
-  Gauge,
-  AllYear,
-  Fall,
-  Spring,
-  Summer,
-  Winter,
-  FallWinter,
-} from '../models';
-import {metricReferenceAs} from '../static/metricReference';
-import {removeNaN} from '../utils/helpers';
-
-let classId = 1;
-const calculatePercentile = async () => {
-  if (classId > 9) {
-    return null;
-  }
-
-  const gauges = await Gauge.findAll({
-    where: {classId},
-    include: [
-      {
-        model: AllYear,
-        as: 'allYears',
-        attributes: ['average', 'standardDeviation', 'coeffientVariance'],
-      },
-      {
-        model: Fall,
-        as: 'falls',
-        attributes: ['timing', 'magnitude', 'timingWet', 'duration'],
-      },
-      {
-        model: Spring,
-        as: 'springs',
-        attributes: ['timing', 'magnitude', 'rateOfChange', 'duration'],
-      },
-      {
-        model: Summer,
-        as: 'summers',
-        attributes: [
-          'timing',
-          'magnitude10',
-          'magnitude50',
-          'durationFlush',
-          'durationWet',
-          'noFlowCount',
-        ],
-      },
-      {
-        model: Winter,
-        as: 'winters',
-        attributes: [
-          'timing2',
-          'timing5',
-          'timing10',
-          'timing20',
-          'timing50',
-          'duration2',
-          'duration5',
-          'duration10',
-          'duration20',
-          'duration50',
-          'frequency2',
-          'frequency5',
-          'frequency10',
-          'frequency20',
-          'frequency50',
-          'magnitude2',
-          'magnitude5',
-          'magnitude10',
-          'magnitude20',
-          'magnitude50',
-        ],
-      },
-      {
-        model: FallWinter,
-        as: 'fallWinters',
-        attributes: ['magWet'],
-      },
-    ],
-  });
-
-  const combined = {};
-  metricReferenceAs.forEach(reference => {
-    combined[reference.short] = [];
-    gauges.forEach(gauge => {
-      if (gauge[reference.as][0]) {
-        combined[reference.short] = [
-          ...combined[reference.short],
-          ...gauge[reference.as][0][reference.columnName],
-        ];
-      }
-    });
-  });
-  const combinedPercentile = {};
-  Object.keys(combined).forEach(key => {
-    const sortedData = removeNaN(combined[key]);
-    combinedPercentile[key] = [
-      d3.quantile(sortedData, 0.1),
-      d3.quantile(sortedData, 0.5),
-      d3.quantile(sortedData, 0.9),
-    ];
-  });
-
-  const updateClass = await Classification.findById(classId);
-  updateClass
-    .update(combinedPercentile, {
-      fields: Object.keys(combinedPercentile),
-    })
-    .then(() => {
-      classId = classId + 1;
-      calculatePercentile(classId);
-    });
-};
+import {Classification, Hydrograph, Gauge} from '../models';
+import {calculatePercentileClourse} from '../utils/helpers';
 
 module.exports = {
   create(req, res) {
@@ -158,7 +43,7 @@ module.exports = {
 
   async update(req, res) {
     try {
-      await calculatePercentile();
+      await calculatePercentileClourse(1);
       res.status(200).send('success');
     } catch (e) {
       res.status(400).send(e.toString());
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,16 @@
 import * as d3 from 'd3';
 import {sortBy, find} from 'lodash';
-import {AllYear, Gauge} from '../models';
+import {
+  AllYear,
+  Gauge,
+  Classification,
+  Fall,
+  Spring,
+  Summer,
+  Winter,
+  FallWinter,
+} from '../models';
+import {metricReferenceAs} from '../static/metricReference';
 
 export const removeNaN = array => {
   const filteredArray = array.filter(ele => !isNaN(Number(ele)));
@@ -125,4 +135,111 @@ export const gaugeNonDimValues = async (req, metric) => {
     }
   });
   return [{[req.body.metric]: nonDimArray}];
+};
+
+export const calculatePercentileClourse = async id => {
+  let classId = id;
+
+  const calculatePercentile = async () => {
+    if (classId > 9) {
+      return null;
+    }
+
+    const gauges = await Gauge.findAll({
+      where: {classId},
+      include: [
+        {
+          model: AllYear,
+          as: 'allYears',
+          attributes: ['average', 'standardDeviation', 'coeffientVariance'],
+        },
+        {
+          model: Fall,
+          as: 'falls',
+          attributes: ['timing', 'magnitude', 'timingWet', 'duration'],
+        },
+        {
+          model: Spring,
+          as: 'springs',
+          attributes: ['timing', 'magnitude', 'rateOfChange', 'duration'],
+        },
+        {
+          model: Summer,
+          as: 'summers',
+          attributes: [
+            'timing',
+            'magnitude10',
+            'magnitude50',
+            'durationFlush',
+            'durationWet',
+            'noFlowCount',
+          ],
+        },
+        {
+          model: Winter,
+          as: 'winters',
+          attributes: [
+            'timing2',
+            'timing5',
+            'timing10',
+            'timing20',
+            'timing50',
+            'duration2',
+            'duration5',
+            'duration10',
+            'duration20',
+            'duration50',
+            'frequency2',
+            'frequency5',
+            'frequency10',
+            'frequency20',
+            'frequency50',
+            'magnitude2',
+            'magnitude5',
+            'magnitude10',
+            'magnitude20',
+            'magnitude50',
+          ],
+        },
+        {
+          model: FallWinter,
+          as: 'fallWinters',
+          attributes: ['magWet'],
+        },
+      ],
+    });
+
+    const combined = {};
+    metricReferenceAs.forEach(reference => {
+      combined[reference.short] = [];
+      gauges.forEach(gauge => {
+        if (gauge[reference.as][0]) {
+          combined[reference.short] = [
+            ...combined[reference.short],
+            ...gauge[reference.as][0][reference.columnName],
+          ];
+        }
+      });
+    });
+    const combinedPercentile = {};
+    Object.keys(combined).forEach(key => {
+      const sortedData = removeNaN(combined[key]);
+      combinedPercentile[key] = [
+        d3.quantile(sortedData, 0.1),
+        d3.quantile(sortedData, 0.5),
+        d3.quantile(sortedData, 0.9),
+      ];
+    });
+
+    const updateClass = await Classification.findById(classId);
+    updateClass
+      .update(combinedPercentile, {
+        fields: Object.keys(combinedPercentile),
+      })
+      .then(() => {
+        classId = classId + 1;
+        calculatePercentile(classId);
+      });
+  };
+  calculatePercentile();
 };
