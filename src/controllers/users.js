093f52e766b21993077fd9f769799fd2b7c9da7d
Leo Q <leoq91@gmail.com>
2018-05-03 14:24:17 -0700

    FF-137: mailgun addition

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,15 @@
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
 const bcrypt = require('bcrypt');
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+};
+
+const nodeMailerMailgun = nodemailer.createTransport(mg(auth));
 
 module.exports = {
   downServer(req, res, io) {
@@ -11,5 +22,25 @@ module.exports = {
     } else {
       return res.status(404).send('Something went wrong!');
     }
+  },
+
+  emailReport(req, res) {
+    const {name, email, msg} = req.body;
+    if (!name || !email || !msg) {
+      return res.status(400).send('Invalid form');
+    }
+
+    const mailOptions = {
+      from: `${name} <${email}>`,
+      to: 'funcflow@gmail.com',
+      subject: 'UC Davis Eflow Bug Report',
+      text: msg,
+    };
+    nodeMailerMailgun.sendMail(mailOptions, error => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send({message: 'Form Submitted!'});
+    });
   },
 };
