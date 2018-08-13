8d1d1eb5ce5d31f42fd45df007459d012b098e5e
Leo Q <leoq91@gmail.com>
2018-08-13 11:42:44 -0700

    deploy

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(logger('tiny'));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors());
+app.use(cors({exposedHeaders: ['Content-Length', 'ff_jwt']}));
 // app.use((req, res, next) => {
 //   const origin = req.headers.origin;
 //   if (allowed_header.indexOf(origin) > -1) {
