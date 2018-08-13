9f956f2714db641280bd1220054614c021394953
Leo Q <leoq91@gmail.com>
2018-08-13 11:34:27 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const CronJob = require('cron').CronJob; //eslint-disable-line
+const cors = require('cors');
 
 const {
   uploadResultToDatabase, //eslint-disable-line
@@ -10,35 +11,36 @@ const {
   uploadGaugeHydrographToDatabase, //eslint-disable-line
 } = require('./utils/uploadToDatabase');
 
-const allowed_header = [
-  'http://localhost:4000',
-  'http://localhost:3000',
-  'http://eflows.ucdavis.edu',
-  'https://eflows.ucdavis.edu',
-  'http://eflow.ucdavis.edu',
-  'https://eflow.ucdavis.edu',
-  'http://environmentalflows.ucdavis.edu',
-  'https://environmentalflows.ucdavis.edu',
-  'http://environmentalflow.ucdavis.edu',
-  'https://environmentalflow.ucdavis.edu',
-];
+// const allowed_header = [
+//   'http://localhost:4000',
+//   'http://localhost:3000',
+//   'http://eflows.ucdavis.edu',
+//   'https://eflows.ucdavis.edu',
+//   'http://eflow.ucdavis.edu',
+//   'https://eflow.ucdavis.edu',
+//   'http://environmentalflows.ucdavis.edu',
+//   'https://environmentalflows.ucdavis.edu',
+//   'http://environmentalflow.ucdavis.edu',
+//   'https://environmentalflow.ucdavis.edu',
+// ];
 
 const app = express();
 
 app.use(logger('tiny'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowed_header.indexOf(origin) > -1) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, FF_JWT, ff_jwt, Accept'
-  );
-  next();
-});
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   if (allowed_header.indexOf(origin) > -1) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, FF_JWT, ff_jwt, Accept'
+//   );
+//   next();
+// });
 
 app.io = require('socket.io')({
   origins: ['http://localhost:3000', 'http://localhost:4000', '*:*'],
