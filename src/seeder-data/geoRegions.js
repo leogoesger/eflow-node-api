18a9067a4d2d16ad8096f3420085d854d468e742
Madhav Parekh <madhavparekh@gmail.com>
2019-03-07 09:19:07 -0800

    FF-408: Updated seeder data for Geo

--- src/seeder-data/geoRegions.js
+++ src/seeder-data/geoRegions.js
@@ -3,7 +3,7 @@ module.exports = [
     id: 1,
     name: 'South Fork Eel',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'The South Fork Eel River basin drains approximately 1785 square kilometers of northern, coastal California. The South Fork Eel River ultimately drains into the mainstem of the Eel River between Weott and Redcrest, CA. The basin provides important cold freshwater habitat for anadromous fish.',
     abbreviation: 'SFE',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
@@ -12,7 +12,7 @@ module.exports = [
     id: 2,
     name: 'North Coast',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'The North Coast region is made up of multiple coastal watersheds including the Eel River, Mad River, and Redwood Creek. In total the region encompasses over 12,500 square kilometers and is dominated by mountainous terrain in addition to the lowlands surrounding Humboldt Bay',
     abbreviation: 'NC',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
@@ -21,7 +21,7 @@ module.exports = [
     id: 3,
     name: 'North Central Coast',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'The North Central Coastal region is over 13,000 square kilometers between San Francisco Bay and the Eel River basin. The majority of the region is within Marin, Napa, Sonoma, and Mendocino Counties where the Russian, Navarro, and Big Rivers are most notable.',
     abbreviation: 'NCC',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
@@ -30,7 +30,7 @@ module.exports = [
     id: 4,
     name: 'South Central Coast',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'The South Central Coast region stretches south from San Francisco Bay until reaching the Santa Maria River basin and includes approximately 26,500 square kilometers. A large proportion of the region flows into Monterey Bay through the Salinas and Pajaro Rivers.',
     abbreviation: 'SCC',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
@@ -39,7 +39,7 @@ module.exports = [
     id: 5,
     name: 'South Coast',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'The South Coast region encompasses nearly 37,000 square kilometers from the Santa Maria River south to the US-Mexico border. This region is the most arid of the coastal California regions and is most impacted by urbanization of rivers and streams. Large rivers in the region include the Santa Maria, Santa Clara, and Santa Ana Rivers.',
     abbreviation: 'SC',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
@@ -48,7 +48,7 @@ module.exports = [
     id: 6,
     name: 'Sacramento',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'The Sacramento River basin encompasses approximately 72,000 square kilometers of diverse terrain. High topographic and climatic differences exist within the basin due to landscapes including the Sierra Nevada, Modoc Plateau, Cascade range, Coastal range, and Central Valley. The Central Vally is dominated by agricultural land use. Ultimately, the Sacramento River drains into the Sacramento-San Joaquin River Delta. Tributaries include the Pit, Feather, and American Rivers.',
     abbreviation: 'SAC',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
@@ -57,7 +57,7 @@ module.exports = [
     id: 7,
     name: 'Klamath',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Including the Butte Creek and Lost River basins, the Klamath River basin encompasses nearly 28,000 square kilometers within California and approximately 15,000 square kilometers in Oregon. The drainage basin includes a range of climatic conditions from arid regions to temperate rainforests of the Pacific coast. Tributaries to the Klamath River include the Trinity, Scott, and Shasta Rivers.',
     abbreviation: 'KLA',
     updatedAt: '2017-11-19T17:25:28.445Z',
     createdAt: '2017-11-19T17:25:28.445Z',
